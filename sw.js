554e28e62482595fd1ab9e55ef010e9800804cc3
honami341 <honami341@gmail.com>
2026-05-13 11:52:16 +0900

    Add offline local video playback

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'lesson-player-v2';
+const APP_SHELL = [
+  './',
+  './index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        if (event.request.url.startsWith(self.location.origin)) {
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request)
+        .then(cached => cached || caches.match('./index.html')))
+  );
+});
